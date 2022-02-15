2a757bb0847a1f05d2ab4f7cc86138af0fe8cfa4
Thomas Boyens <60038450+ThomasBoyens@users.noreply.github.com>
2022-02-15 19:14:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://xenodochial-gates-cf6795.netlify.app'
+        origin: 'https://xenodochial-gates-cf6795.netlify.app',
+        methods: GET,
+        headers: Content-Type
     })
 );
 app.options('*', cors());
